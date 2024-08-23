addd1dffb0a11ee80cef51eadc3a2bc2d74a28d9
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 21:57:21 +0200

    improve json-deep-each to handle nested structures and add tests

--- spec/json_deep_each_spec.rb
+++ spec/json_deep_each_spec.rb
@@ -2,5 +2,13 @@
 
 require "spec_helper"
 
-RSpec.describe "each" do
+RSpec.describe "json-deep-each" do
+  it "works" do
+    expect(`echo [1,2,3] | bin/json-deep-each "p it"`).to eq(<<~OUTPUT)
+    [1, 2, 3]
+    1
+    2
+    3
+    OUTPUT
+  end
 end
