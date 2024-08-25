1d7b547a49556de7b4a1784c4dd93c2d67f452ad
Dorian Marié <dorian@dorianmarie.com>
2024-08-25 20:09:43 +0200

    update gem dependencies and configuration files

--- spec/json_deep_each_spec.rb
+++ spec/json_deep_each_spec.rb
@@ -5,10 +5,10 @@ require "spec_helper"
 RSpec.describe "json-deep-each" do
   it "works" do
     expect(`echo [1,2,3] | bin/json-deep-each "p it"`).to eq(<<~OUTPUT)
-    [1, 2, 3]
-    1
-    2
-    3
+      [1, 2, 3]
+      1
+      2
+      3
     OUTPUT
   end
 end
